5e8b43b396d94e89bf78adf35715c82a18241d30
Laura Mambrilla Moreno <lauramambrilla@gmail.com>
2024-05-06 15:13:39 +0200

    Sprint

--- SweetFrenzy/Assets/Sprites/Player.cs
+++ SweetFrenzy/Assets/Sprites/Player.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// PlayerID options
-/// </summary>
 public enum PlayerID
 {
     player1 = 1,
@@ -13,9 +10,22 @@ public enum PlayerID
 
 public class Player : MonoBehaviour
 {
+    [Header("Player settings")]
     [SerializeField] private PlayerID playerID;
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float rotationSpeed = 5f;
+
+    [Header("Movement settings")]
+    [SerializeField] private float speed = 5f;                  // Velocidad de movimiento 
+    [SerializeField] private float normalSpeed = 5f;            // Velocidad normal
+    [SerializeField] private float rotationSpeed = 5f;          // Velocidad de rotacion
+
+    [Header("Sprint settings")]
+    [SerializeField] private float sprintSpeed = 10f;           // Velocidad de sprint
+    [SerializeField] private float sprintDuration = 3f;         // Duración del sprint en segundos
+    [SerializeField] private float sprintCooldown = 5f;         // Tiempo de espera entre sprints en segundos
+    [SerializeField] private bool isSprinting = false;
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private float sprintTimer = 0f;            // Temporizador del sprintDuration
+    [SerializeField] private float sprintCooldownTimer = 0f;    // Temporizador del sprintCooldown
 
     // Start is called before the first frame update
     void Start()
@@ -29,10 +39,19 @@ public class Player : MonoBehaviour
         Move();
     }
 
-    /// <summary>
-    /// Player's movement
-    /// </summary>
     private void Move()
+    {
+        Movement();
+        Sprint();
+        UpdateSprint();
+    }
+
+    #region Movement and rotation
+
+    /// <summary>
+    /// Player's walk movement
+    /// </summary>
+    private void Movement()
     {
         float horizontalInput;
         float verticalInput;
@@ -42,30 +61,93 @@ public class Player : MonoBehaviour
             horizontalInput = Input.GetAxis("Horizontal_P1");
             verticalInput = Input.GetAxis("Vertical_P1");
         }
-        else 
+        else
         {
             horizontalInput = Input.GetAxis("Horizontal_P2");
             verticalInput = Input.GetAxis("Vertical_P2");
         }
-        
+
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
 
-        transform.position = transform.position + movementDirection * speed * Time.deltaTime;
-        
-        Rotate(movementDirection);
+        transform.position = transform.position + movementDirection * (isSprinting ? sprintSpeed : speed) * Time.deltaTime;
+
+        Rotation(movementDirection);
     }
 
     /// <summary>
     /// Player's rotation based on direction of movement
     /// </summary>
     /// <param name="movementDirection"></param>
-    private void Rotate(Vector3 movementDirection)
+    private void Rotation(Vector3 movementDirection)
     {
         if (movementDirection != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), rotationSpeed * Time.deltaTime);
         }
     }
+
+    #endregion
+
+    #region Sprint
+
+    /// <summary>
+    /// Sprint functionality
+    /// </summary>
+    private void Sprint()
+    {
+        bool sprintInput;
+        if (playerID == PlayerID.player1)
+        {
+            sprintInput = Input.GetKeyDown(KeyCode.LeftShift);
+        }
+        else
+        {
+            sprintInput = Input.GetKeyDown(KeyCode.RightShift);
+        }
+
+        if (sprintInput)
+        {
+            if (!isSprinting && canSprint)
+            {
+                isSprinting = true;
+                canSprint = false;
+                speed = sprintSpeed;
+                sprintTimer = sprintDuration;
+            }
+        }        
+    }
+
+    /// <summary>
+    /// Update sprint settings
+    /// </summary>
+    private void UpdateSprint()
+    {
+        if (isSprinting)
+        {
+            sprintTimer -= Time.deltaTime;
+            
+            if (sprintTimer <= 0f)
+            {
+                isSprinting = false;
+                sprintCooldownTimer = sprintCooldown;
+                speed = normalSpeed;
+            }
+        }
+        else
+        {
+            if (sprintCooldownTimer <= 0f)
+            {
+                canSprint = true;
+            }
+            else
+            {
+                sprintCooldownTimer -= Time.deltaTime;
+                canSprint = false;
+            } 
+        }
+    }
+
+    #endregion
 }
