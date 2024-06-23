40184480ff512ce00cde3175c619c4f6ef07589e
Sandra707 <sandyescribano09@gmail.com>
2024-06-23 14:54:08 +0200

    Clients
    Added client logic

---
+++ SweetFrenzy/Assets/Scripts/ClientManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientManager : MonoBehaviour
+{
+    [SerializeField] GameObject clientPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InstantiateClient());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator InstantiateClient()
+    {
+        while (true)
+        {
+            Instantiate(clientPrefab, transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(25f);
+        }
+    }
+}
