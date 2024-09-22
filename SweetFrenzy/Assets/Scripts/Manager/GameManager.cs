17b57b83b9bf947319f1bae90f67ae7c85c0a66e
Laura Mambrilla Moreno <lauramambrilla@gmail.com>
2024-09-22 19:38:38 +0200

    Timer

--- SweetFrenzy/Assets/Scripts/Manager/GameManager.cs
+++ SweetFrenzy/Assets/Scripts/Manager/GameManager.cs
@@ -4,26 +4,65 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private int numOrders = 0;
-    private int points = 0;
-    private bool isGameOver;
+    private int numOrders = 0; // Número de órdenes completadas
+    private int points = 0; // Puntos acumulados
+    private bool isGameOver; // Estado del juego
 
-    // Start is called before the first frame update
+    // Puntuación mínima para ganar
+    private const int winningScore = 50;
+
     void Start()
     {
         isGameOver = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    #region Game Over Functions
+    private void CheckGameResult()
+    {
+        if (points >= winningScore)
+        {
+            PlayerWon();
+        }
+        else
+        {
+            PlayerLost();
+        }
+    }
+
+    private void PlayerWon()
+    {
+        Debug.Log("¡Felicidades! Has ganado la partida con " + points + " puntos.");
+        ShowEndGameScreen(true); 
+    }
+
+    private void PlayerLost()
+    {
+        Debug.Log("Lo siento, no has alcanzado la puntuación mínima. Has perdido la partida.");
+        ShowEndGameScreen(false);
+    }
+
+    private void ShowEndGameScreen(bool hasWon)
+    {
+        if (hasWon)
+        {
+            Debug.Log("Mostrando pantalla de victoria.");
+        }
+        else
+        {
+            Debug.Log("Mostrando pantalla de derrota.");
+        }
+    }
+    #endregion
 
     #region Getters and Setters
     public void AddPoints(int addedPoints)
     {
-        points = points + addedPoints;
+        points += addedPoints;
         Debug.Log("Total points: " + points);
     }
 
@@ -43,5 +82,14 @@ public class GameManager : MonoBehaviour
         return isGameOver;
     }
 
+    public void SetGameOver(bool gameOver)
+    {
+        isGameOver = gameOver;
+
+        if (isGameOver)
+        {
+            CheckGameResult();
+        }
+    }
     #endregion
 }
